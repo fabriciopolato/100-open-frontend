f5cd2c7b9f57fb3a6e2aef53dbc543fa48a3f600
Fabricio Polato <fabriciopolato@gmail.com>
2020-09-07 18:34:05 -0300

    refactor: change Input style

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,5 +1,17 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
 
+  label {
+    margin-bottom: 5px;
+    font-size: 1.5rem;
+  }
+
+  input {
+    height: 43px;
+    font-size: 1rem;
+  }
 `;
