e102d0eef26693b7055b7d7aa4859e2750277556
Fabricio Polato <fabriciopolato@gmail.com>
2020-09-07 12:35:00 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import Routes from './routes';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const App: React.FC = () => <div />;
+const App: React.FC = () => (
+    <Router>
+        <Routes />
+    </Router>
+);
 
 export default App;
