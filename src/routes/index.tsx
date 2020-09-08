06c8480aaf30629fdd38a6a331df72d56f78cc30
Fabricio Polato <fabriciopolato@gmail.com>
2020-09-07 22:28:47 -0300

    refactor: add AddStartup and EditStartup to Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Home from '../pages/Home';
+import { Home, AddStartup, EditStartup } from '../pages';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/criar-startup" component={AddStartup} />
+    <Route exact path="/editar/:id" component={EditStartup} />
   </Switch>
 );
 
