8e9449b6da7661d33ceab523f682b7970b19b42a
Fabricio Polato <fabriciopolato@gmail.com>
2020-09-09 13:18:02 -0300

    feat: add routes to Signup and Login pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Home, AddStartup, EditStartup } from '../pages';
+import { Home, AddStartup, EditStartup, Signup, Login } from '../pages';
 
 const Routes: React.FC = () => (
   <Switch>
+    <Route exact path="/signup" component={Signup} />
+    <Route exact path="/login" component={Login} />
     <Route exact path="/" component={Home} />
     <Route exact path="/criar-startup" component={AddStartup} />
     <Route exact path="/editar/:id" component={EditStartup} />
