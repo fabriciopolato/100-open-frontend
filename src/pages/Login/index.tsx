8ab2da4db73a1dfb8dea951014cfcd1794b52f80
Fabricio Polato <fabriciopolato@gmail.com>
2020-09-09 13:17:33 -0300

    refactor: Login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,52 @@
 import React from 'react';
 
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import { Container } from './styles';
+import { SignForm } from '../../components';
 
-const Login: React.FC = () => (
-  <Container>
-    <h1>Login</h1>
-  </Container>
-);
+const Login: React.FC = () => {
+  const {
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    values,
+    errors,
+    touched,
+    isSubmitting,
+    resetForm,
+    status,
+    setStatus,
+  } = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    onSubmit: () => {
+      console.log(values, 'deu bom!');
+    },
+    validationSchema: yup.object().shape({
+      username: yup.string().required('Username é obrigatório'),
+      password: yup
+        .string()
+        .required('Senha é obrigatória')
+        .min(6, 'Mínimo de 6 dígitos'),
+    }),
+  });
+
+  return (
+    <Container>
+      <SignForm
+        buttonText="Entrar"
+        handleChange={handleChange}
+        handleBlur={handleBlur}
+        handleSubmit={handleSubmit}
+        values={values}
+        errors={errors}
+        touched={touched}
+      />
+    </Container>
+  );
+};
 
 export default Login;
