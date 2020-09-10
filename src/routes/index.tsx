3eb28f2d18283080230b1f3dc50fa38fb5a459ac
Fabricio Polato <fabriciopolato@gmail.com>
2020-09-10 10:23:17 -0300

    refactor: change Route to PrivateRoute

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Home, AddStartup, EditStartup, Signup, Login } from '../pages';
+import PrivateRoute from './PrivateRoute';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/signup" component={Signup} />
     <Route exact path="/login" component={Login} />
-    <Route exact path="/" component={Home} />
-    <Route exact path="/criar-startup" component={AddStartup} />
-    <Route exact path="/editar/:id" component={EditStartup} />
+    <PrivateRoute exact path="/" component={Home} />
+    <PrivateRoute exact path="/criar-startup" component={AddStartup} />
+    <PrivateRoute exact path="/editar/:id" component={EditStartup} />
   </Switch>
 );
 
