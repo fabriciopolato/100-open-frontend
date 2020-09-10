15c37098cb11b3d47f3ecc32cb3a03bc1f011019
Fabricio Polato <fabriciopolato@gmail.com>
2020-09-10 13:09:26 -0300

    feat: add useFormik and call to api

--- src/components/Forms/AddForm/index.tsx
+++ src/components/Forms/AddForm/index.tsx
@@ -1,19 +1,76 @@
 import React from 'react';
 
+import { useHistory } from 'react-router-dom';
+import { useFormik } from 'formik';
 import { Container } from './styles';
 import Form from '../Form';
 import { Input, Button } from '../..';
+import { api } from '../../../services/api';
 
-const AddForm: React.FC = () => (
-  <Container>
-    <Form withBackArrow>
-      <Input name="name" type="text" label="Nome da startup" />
-      <Input name="description" type="text" label="Descrição" />
-      <Input name="type" type="text" label="Tipo" />
-      <Input name="location" type="text" label="Localização" />
-      <Button type="submit">adicionar startup</Button>
-    </Form>
-  </Container>
-);
+const AddForm: React.FC = () => {
+  const history = useHistory();
+
+  const { handleChange, handleBlur, handleSubmit, values } = useFormik({
+    initialValues: {
+      name: '',
+      description: '',
+      type: '',
+      location: '',
+    },
+    onSubmit: async ({ name, description, type, location }) => {
+      try {
+        await api.post(`/company`, {
+          name,
+          description,
+          type,
+          location,
+        });
+        history.push('/');
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit} withBackArrow>
+        <Input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          name="name"
+          type="text"
+          label="Nome da startup"
+          value={values.name}
+        />
+        <Input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          name="description"
+          type="text"
+          label="Descrição"
+          value={values.description}
+        />
+        <Input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          name="type"
+          type="text"
+          label="Tipo"
+          value={values.type}
+        />
+        <Input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          name="location"
+          type="text"
+          label="Localização"
+          value={values.location}
+        />
+        <Button type="submit">adicionar startup</Button>
+      </Form>
+    </Container>
+  );
+};
 
 export default AddForm;
--- src/components/Forms/EditForm/index.tsx
+++ src/components/Forms/EditForm/index.tsx
@@ -1,27 +1,81 @@
 import React from 'react';
 
+import { useFormik } from 'formik';
+import { useHistory } from 'react-router-dom';
 import { Container, StartupInfo, Form } from './styles';
 import { Input, Button, Header } from '../..';
+import { useCompany } from '../../../hooks/company';
+import { api } from '../../../services/api';
 
-const EditForm: React.FC = () => (
-  <Container>
-    <Header withBackArrow />
-    <StartupInfo>
-      <h2>Nome da Empresa</h2>
-      <p>Descrição</p>
-      <Button type="submit" backgroundColor="#f24545">
-        marcar como inativa
-      </Button>
-    </StartupInfo>
-    <Form>
-      <fieldset>
-        <legend>editar os dados da startup</legend>
-        <Input name="type" type="text" label="Tipo" />
-        <Input name="location" type="text" label="Localização" />
-      </fieldset>
-      <Button type="submit">editar startup</Button>
-    </Form>
-  </Container>
-);
+const EditForm: React.FC = () => {
+  const { handleInactivity, currentCompany } = useCompany();
+
+  const { name, type, description, _id, votesForInactivity } = currentCompany;
+  const userId = localStorage.getItem('open:userId');
+
+  const history = useHistory();
+
+  const { handleChange, handleBlur, handleSubmit, values } = useFormik({
+    initialValues: {
+      companyType: type,
+      companyLocation: '',
+    },
+    onSubmit: async ({ companyType, companyLocation }) => {
+      try {
+        await api.put(`/company/${_id}`, {
+          type: companyType,
+          location: companyLocation,
+        });
+        history.push('/');
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+
+  return (
+    <Container>
+      <Header withBackArrow />
+      <StartupInfo>
+        <h2>{name}</h2>
+        <p>
+          <span>{type}</span>
+        </p>
+        <p>{description}</p>
+        {!votesForInactivity.includes(userId as string) && (
+          <Button
+            onClick={() => handleInactivity(_id)}
+            type="button"
+            backgroundColor="#f24545"
+          >
+            marcar como inativa
+          </Button>
+        )}
+      </StartupInfo>
+      <Form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>editar os dados da startup</legend>
+          <Input
+            onChange={handleChange}
+            onBlur={handleBlur}
+            name="companyType"
+            type="text"
+            label="Tipo"
+            value={values.companyType}
+          />
+          <Input
+            onChange={handleChange}
+            onBlur={handleBlur}
+            name="companyLocation"
+            type="text"
+            label="Localização"
+            value={values.companyLocation}
+          />
+        </fieldset>
+        <Button type="submit">editar startup</Button>
+      </Form>
+    </Container>
+  );
+};
 
 export default EditForm;
