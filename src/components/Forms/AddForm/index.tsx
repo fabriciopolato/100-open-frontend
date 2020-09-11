b8f355cb5c0663627b96cf113cf94d3d178258b7
Fabricio Polato <fabriciopolato@gmail.com>
2020-09-10 23:14:34 -0300

    feat: get locations from geoNames api

--- src/components/Forms/AddForm/index.tsx
+++ src/components/Forms/AddForm/index.tsx
@@ -1,19 +1,63 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
 import { useHistory } from 'react-router-dom';
 import { useFormik } from 'formik';
 import BounceLoader from 'react-spinners/BounceLoader';
+import * as yup from 'yup';
 import { Container, StyledLoader } from './styles';
 import Form from '../Form';
 import { Input, Button } from '../..';
-import { api } from '../../../services/api';
+import { api, geoNamesApi } from '../../../services/api';
 import { useCompany } from '../../../hooks/company';
+
+interface IGeoLocation {
+  name: string;
+  countryName: string;
+  adminCodes1: {
+    ISO3166_2: string;
+  };
+}
 
 const AddForm: React.FC = () => {
   const history = useHistory();
   const { isLoading, setIsLoading } = useCompany();
 
-  const { handleChange, handleBlur, handleSubmit, values } = useFormik({
+  const [geoNamesCities, setGeoNamesCities] = useState<IGeoLocation[]>([]);
+
+  const handleGeoNamesApi = async (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setGeoNamesCities([]);
+    try {
+      if (value.length >= 3) {
+        const response = await geoNamesApi.get('', {
+          params: { q: value },
+        });
+
+        const formattedData = response.data.geonames.map(
+          (location: IGeoLocation) => {
+            return {
+              name: location.name,
+              countryName: location.countryName,
+              adminCodes1: location.adminCodes1.ISO3166_2,
+            };
+          },
+        );
+
+        setGeoNamesCities(formattedData);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const {
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    values,
+    errors,
+    touched,
+  } = useFormik({
     initialValues: {
       name: '',
       description: '',
@@ -23,7 +67,7 @@ const AddForm: React.FC = () => {
     onSubmit: async ({ name, description, type, location }) => {
       setIsLoading(true);
       try {
-        await api.post(`/company`, {
+        await api.post('/company', {
           name,
           description,
           type,
@@ -37,6 +81,14 @@ const AddForm: React.FC = () => {
         history.push('/');
       }, 1500);
     },
+    validationSchema: yup.object().shape({
+      name: yup.string().required('Nome é um campo obrigatório'),
+      description: yup
+        .string()
+        .max(140, 'Você ultrapassou o limite de 140 caracteres'),
+      type: yup.string().required('Tipo é um campo obrigatório'),
+      location: yup.string().required('Localização é um campo obrigatório'),
+    }),
   });
 
   return (
@@ -50,6 +102,7 @@ const AddForm: React.FC = () => {
           label="Nome da startup"
           value={values.name}
         />
+        {errors.name && touched.name ? <p>{errors.name}</p> : null}
         <Input
           onChange={handleChange}
           onBlur={handleBlur}
@@ -58,6 +111,10 @@ const AddForm: React.FC = () => {
           label="Descrição"
           value={values.description}
         />
+        {errors.description && touched.description ? (
+          <p>{errors.description}</p>
+        ) : null}
+
         <Input
           onChange={handleChange}
           onBlur={handleBlur}
@@ -66,14 +123,22 @@ const AddForm: React.FC = () => {
           label="Tipo"
           value={values.type}
         />
+        {errors.type && touched.type ? <p>{errors.type}</p> : null}
+
         <Input
-          onChange={handleChange}
+          onChange={event => {
+            handleChange(event);
+            handleGeoNamesApi(event);
+          }}
           onBlur={handleBlur}
           name="location"
           type="text"
           label="Localização"
           value={values.location}
         />
+        {}
+        {errors.location && touched.location ? <p>{errors.location}</p> : null}
+
         {isLoading ? (
           <StyledLoader>
             <BounceLoader size={60} color="#45aaf2" />
