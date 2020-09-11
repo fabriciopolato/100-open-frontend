e7593556f7e2265e31d390be7d2d8d8eb2801d5a
Fabricio Polato <fabriciopolato@gmail.com>
2020-09-10 23:14:01 -0300

    feat: create geoNamesApi baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,3 +3,13 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
+
+export const geoNamesApi = axios.create({
+  baseURL: process.env.REACT_APP_GEO_NAMES_API_URL,
+  params: {
+    username: process.env.REACT_APP_USERNAME_GEO_NAMES,
+    type: 'json',
+    maxRows: 10,
+    featureClass: 'P',
+  },
+});
