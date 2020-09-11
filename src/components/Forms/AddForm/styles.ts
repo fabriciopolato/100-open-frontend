b0840482504b51356aaa80dc87dd016de5bb4462
Fabricio Polato <fabriciopolato@gmail.com>
2020-09-10 23:12:52 -0300

    refactor: change styles

--- src/components/Forms/AddForm/styles.ts
+++ src/components/Forms/AddForm/styles.ts
@@ -2,6 +2,12 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   min-width: 300px;
+
+  p {
+    margin-top: -13px;
+    margin-bottom: 30px;
+    color: #f24545;
+  }
 `;
 
 export const StyledLoader = styled.div`
